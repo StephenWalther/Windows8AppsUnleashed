1a38096fbb8bcec7f419f873b7d0b4ded57dbdd1
StephenWalther <Stephen.Walther@superexpert.com>
2012-07-22 11:06:16 -0700

    More chapter 8 code

---
+++ Chapter08/Chapter08/Chapter08/messageDialog/messageDialog.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    "use strict";
+
+    function initialize() {
+        var message = new Windows.UI.Popups.MessageDialog(
+            "Did you know that your fly is unzipped?"
+        );
+        message.showAsync();
+    }
+
+    document.addEventListener("DOMContentLoaded", initialize);
+})();
---
+++ Chapter08/Chapter08/Chapter08/messageDialogYesNo/messageDialogYesNo.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    "use strict";
+
+    function initialize() {
+        // Create dialog
+        var message = new Windows.UI.Popups.MessageDialog(
+            "Did you know that your fly is unzipped?",
+            "Warning!!!"
+        );
+
+        // Add commands
+        message.commands.append(new Windows.UI.Popups.UICommand("&Yes"));
+        message.commands.append(new Windows.UI.Popups.UICommand("&No"));
+
+        // Show dialog
+        message.showAsync().done(function(answer) {
+            if (answer.label === "&Yes") {
+                console.log("You picked yes");
+            } else {
+                console.log("You picked no");
+            }
+        });
+    }
+
+    document.addEventListener("DOMContentLoaded", initialize);
+})();
