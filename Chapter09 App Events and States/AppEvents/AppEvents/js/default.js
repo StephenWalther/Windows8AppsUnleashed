3d5faa5fe90089d6ab02b81e9f955e3068545f64
StephenWalther <Stephen.Walther@superexpert.com>
2012-07-27 13:07:57 -0700

    Renamed projects to be chapter # independent

--- Chapter09 App Events and States/AppEvents/AppEvents/js/default.js
+++ Chapter09 App Events and States/AppEvents/AppEvents/js/default.js
@@ -1,35 +1,33 @@
-﻿/// <reference path="//Microsoft.WinJS.1.0.RC/js/base.js" />
-/// <reference path="//Microsoft.WinJS.1.0.RC/js/ui.js" />
-
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
 (function () {
     "use strict";
 
-    var _gameScore;
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+    WinJS.strictProcessing();
 
-    WinJS.Application.addEventListener("activated", function (evt) {
-        var appState = Windows.ApplicationModel.Activation.ApplicationExecutionState;
-        if (evt.detail.previousExecutionState == appState.notRunning ||
-            evt.detail.previousExecutionState == appState.closedByUser) {
-            // Set default game score
-            _gameScore = 0;
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                // TODO: This application has been newly launched. Initialize
+                // your application here.
+            } else {
+                // TODO: This application has been reactivated from suspension.
+                // Restore application state here.
+            }
+            args.setPromise(WinJS.UI.processAll());
         }
-        if (evt.detail.previousExecutionState == appState.terminated) {
-            // Load game score from session state
-            _gameScore = WinJS.Application.sessionState.gameScore;
-        }
-    });
+    };
 
-    WinJS.Application.addEventListener("checkpoint", function () {
-        // Save game score to session state
-        WinJS.Application.sessionState.gameScore = _gameScore;
-    });
+    app.oncheckpoint = function (args) {
+        // TODO: This application is about to be suspended. Save any state
+        // that needs to persist across suspensions here. You might use the
+        // WinJS.Application.sessionState object, which is automatically
+        // saved and restored across suspension. If you need to complete an
+        // asynchronous operation before your application is suspended, call
+        // args.setPromise().
+    };
 
-    WinJS.Application.addEventListener("ready", function (evt) {
-        // Killed alien, +1 to game score
-        document.getElementById("btnKillAlien").addEventListener("click", function () {
-            _gameScore++;
-        });
-    });
-
-    WinJS.Application.start();
+    app.start();
 })();
