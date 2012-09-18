05e24610da3fe2b61a407af72b5c27979c821a6b
StephenWalther <Stephen.Walther@superexpert.com>
2012-09-17 21:33:29 -0700

    Update Chapter 5

---
+++ Chapter05 Creating Forms/Forms/Forms/inputFile/inputFile.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    "use strict";
+
+    function initialize() {
+
+        var frmAdd = document.getElementById("frmAdd");
+
+        frmAdd.addEventListener("submit", function (evt) {
+            evt.preventDefault();
+
+            var imgPicture = document.getElementById("imgPicture");
+            var inpFile = document.getElementById("inpFile");
+
+            if (inpFile.files.length > 0) {
+                // Use HTML5 File API to create object URL to refer to the photo file
+                var pictureUrl = URL.createObjectURL(inpFile.files[0]);
+
+                // Show photo in IMG element
+                imgPicture.src = pictureUrl;
+            }
+        });
+
+    }
+
+    document.addEventListener("DOMContentLoaded", initialize);
+})();
